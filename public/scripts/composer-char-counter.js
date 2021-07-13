53776840a5d89eef0f28f248195d1045e8e9efe5
mohammedpythons <mohammedshokai99@gmail.com>
2021-07-13 16:15:01 -0400

    adding fixed nav

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,13 +4,13 @@ $(document).ready(() => {
     let inputLength = $(".tweet-text").val()
 		$(this).siblings().children(".counter");
 		if (inputLength) {
-			let  counter =  141 - inputLength.length;
+			let  counter = 140 - inputLength.length;
 			$(this).siblings().children(".counter").val(counter);
 			if (counter < 0) {
 				$(this).siblings().children(".counter").css("color", "red");
 			} else {
 				$(this).siblings().children(".counter").css("color", "black");
-			}
+			} 
 			
 		}
 
